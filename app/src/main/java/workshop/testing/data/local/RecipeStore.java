e8b2569d10e60823c4eb55a789dcf223e1b4e867
THE_One\Admin <konnata_D19@hotmail.com>
2018-04-29 02:17:39 +0700

    convert from https://github.com/kanjanaroot/workshopAndroid.git

--- app/src/main/java/workshop/testing/data/local/RecipeStore.java
+++ app/src/main/java/workshop/testing/data/local/RecipeStore.java
@@ -7,8 +7,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Handler;
 
 import workshop.testing.data.model.Recipe;
 
@@ -20,16 +22,26 @@ public class RecipeStore {
         // TODO:: for your homework -> Read all file from assets/recipes directory
 
         // Demo in workshop :: Ugly code !!
-        File file1 = new File(directory, "chocolate_pudding.txt");
-        InputStream stream1 = null;
+//        File file1 = new File(directory, "chocolate_pudding.txt");
+        InputStream stream = null;
+
+        List<File> fileList=new LinkedList<>();
+        fileList.add(new File(directory, "chocolate_pudding.txt"));
+        fileList.add(new File(directory, "creamed_carrots.txt"));
+        fileList.add(new File(directory, "deviled_eggs.txt"));
+        fileList.add(new File(directory, "fried_tomatoes.txt"));
         try {
-            stream1 = context.getAssets().open(file1.getPath());
-            Recipe recipe = Recipe.readFromStream(stream1);
-            recipes.add(recipe);
-            map.put(recipe.id, recipe);
+            for (int i=0; i<fileList.size();i++){
+                stream = context.getAssets().open(fileList.get(i).getPath());
+                Recipe recipe = Recipe.readFromStream(stream);
+                recipes.add(recipe);
+                map.put(recipe.id, recipe);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     public Recipe getRecipe(String id) {
